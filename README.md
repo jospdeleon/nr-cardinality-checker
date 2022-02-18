970266fff7aa49cec16d435072c3a2ef92ded2d1
Jo Ann S de Leon <jdeleon@newrelic.com>
2022-02-18 13:15:07 -0500

    initial version

---
+++ README.md
@@ -0,0 +1,47 @@
+# nr-cardinality-checker
+
+### A Scripted API test to check current cardinality both at the account and metrics levels.
+
+
+## Installation
+
+> From root directory, run `npm install` to install node modules.
+
+## Usage
+
+> The script is using two types of thresholds:
+
+1. ACCOUNT_THRESHOLD is currently set to 5,000,000
+2. METRIC_THRESHOLD is currently set to 50,000
+
+> These can be changed to whatever thresholds are needed. When either of these are reached, the script will fail. You can then set up an alert if this monitor fails to get notified when there are any "offending" metrics with high cardinality.
+
+## Running the Script Locally
+
+> Add this to the top of your script, the library mimics the Synthetics API and implements most common commands in Synthetics.
+
+```
+    if (typeof $env === "undefined" || $env === null) {
+      global._isApiTest = true;  // false, for Scripted Browser
+      require("../lib/simulator");
+    }
+```
+
+**NOTE:** Make sure to remove these lines when you copy the script to a Synthetic monitor in the New Relic UI.
+
+> Set up the following variables in `lib/simulator.js`:
+
+1. Replace the `ACCOUNT_ID` attribute in the `env` variable with the account ID that you want to check for cardinality.
+
+2. Replace the `GRAPHQL_KEY` attribute in the `secure` variable with a user API key to call NerdGraph.
+
+> To run, execute the command `node cardinality_limits.js`. 
+
+
+If no thresholds are reached, the script will print a line:
+
+```
+No thresholds reached!
+```
+
+Otherwise, the script will fail with an `AssertionError` showing the account cardinality and/or each metric cardinality that crossed the thresholds.
---
+++ cardinality_limits.js
@@ -0,0 +1,114 @@
+/**
+ * For local development
+ */
+ if (typeof $env === "undefined" || $env === null) {
+  global._isApiTest = true;
+  require("./lib/simulator");
+}
+
+var assert = require('assert');
+
+//SET THE FOLLOWING TO DESIRED THRESHOLDS
+const ACCOUNT_THRESHOLD = 5000000; //30% of 15M account limit
+const METRIC_THRESHOLD = 50000; //50% of 100k metric limit
+
+const getAccountLimit = () => {
+  return new Promise((resolve, reject) => {
+    console.log('Fetching account cardinality...')
+    const options = {
+      url: 'https://api.newrelic.com/graphql',
+      body: JSON.stringify({"query":`{
+        actor {
+          account(id: ${$env.ACCOUNT_ID}) {
+            nrql(query: "SELECT cardinality() FROM Metric WHERE metricName NOT LIKE '%newrelic%' SINCE today WITH TIMEZONE 'UTC' RAW", timeout: 20) {
+              results
+            }
+          }
+        }
+      }
+      `,"variables":null}),
+      headers: {
+        "api-key": $secure.GRAPHQL_KEY,
+        "content-type": "application/json"
+      }
+    }
+
+    $http.post(options, (err, response, body) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      assert.equal(response.statusCode, 200, `Expected a 200 OK response, received ${response.statusCode}`);
+      const { data } = JSON.parse(body);
+
+      const { nrql } = data.actor.account
+      let cardinality = nrql ? nrql.results[0]["cardinality.null"] : 0;
+      console.log(`Current Account cardinality: ${cardinality}`)
+      resolve(cardinality)
+
+    })
+  });
+}
+
+const getMetricLimits = () => {
+  return new Promise((resolve, reject) => {
+    console.log('Fetching metrics cardinality...')
+    const options = {
+      url: 'https://api.newrelic.com/graphql',
+      body: JSON.stringify({"query":`{
+        actor {
+          account(id: ${$env.ACCOUNT_ID}) {
+            nrql(query: "SELECT cardinality() FROM Metric FACET metricName WHERE metricName NOT LIKE '%newrelic%' SINCE today WITH TIMEZONE 'UTC' RAW LIMIT 200", timeout: 30) {
+              results
+            }
+          }
+        }
+      }
+      `,"variables":null}),
+      headers: {
+        "api-key": $secure.GRAPHQL_KEY,
+        "content-type": "application/json"
+      }
+    }
+
+    $http.post(options, (err, response, body) => {
+      if (err) {
+        reject(err)
+        return
+      }
+
+      assert.equal(response.statusCode, 200, `Expected a 200 OK response, received ${response.statusCode}`);
+      const { data } = JSON.parse(body);
+      const { nrql } = data.actor.account
+
+      let results = nrql ? nrql.results : [];
+      resolve(results)
+
+    })
+  })
+}
+
+const breaches = []
+
+getAccountLimit().then(cardinality => {
+  if (cardinality >= ACCOUNT_THRESHOLD) {
+    breaches.push(`Account: ${cardinality}`)
+  }
+
+  // assert.ok(cardinality < ACCOUNT_THRESHOLD, "Current cardinality has reached threshold")
+  getMetricLimits().then(results => {
+    if (results && results.length > 0) {
+
+      // Check if each metric has crossed the threshold
+      for (const metric of results) {
+        if (metric['cardinality.null'] >= METRIC_THRESHOLD) {
+          breaches.push(`${metric.metricName}: ${metric['cardinality.null']}`)
+        }
+      }
+    }
+
+    assert.equal(breaches.length, 0, `One or more metrics reached the threshold:\n${breaches.join('\n')}`)
+    console.log("No thresholds reached!")
+  })
+})
